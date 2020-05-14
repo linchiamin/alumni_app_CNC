98c9dc3a4315e7da7e3bfb7d4477cbe3e35cf576
linchiamin <58568688+linchiamin@users.noreply.github.com>
2020-05-14 15:11:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
 	# here we use 'render_template' to link to the html template
-	return render_template('home.html')
+	return render_static('index2.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
